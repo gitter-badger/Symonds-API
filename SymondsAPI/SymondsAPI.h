3e2537ed7000cf139814752bb741973cfad62697
Søren Mortensen <soren@sorenmortensen.com>
2016-06-22 14:06:42 +0100

    Add project

---
+++ SymondsAPI/SymondsAPI.h
@@ -0,0 +1,19 @@
+//
+//  SymondsAPI.h
+//  SymondsAPI
+//
+//  Created by Søren Mortensen on 22/06/2016.
+//  Copyright © 2016 Soren Mortensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SymondsAPI.
+FOUNDATION_EXPORT double SymondsAPIVersionNumber;
+
+//! Project version string for SymondsAPI.
+FOUNDATION_EXPORT const unsigned char SymondsAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SymondsAPI/PublicHeader.h>
+
+
