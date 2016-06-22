e53ed4d4972a6d8c61f8ebe29b69d04cce6ae158
Søren Mortensen <soren@sorenmortensen.com>
2016-06-22 18:10:08 +0100

    Implement some test code to verify that Cocoapods is working correctly

--- SymondsAPI.podspec
+++ SymondsAPI.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "SymondsAPI"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "An API to access information from data.psc.ac.uk"
   s.description  = <<-DESC
                     This is an API for accessing information through data.psc.ac.uk
---
+++ SymondsAPI/SymondsAPI.swift
@@ -0,0 +1,17 @@
+//
+//  SymondsAPI.swift
+//  SymondsAPI
+//
+//  Created by Søren Mortensen on 22/06/2016.
+//  Copyright © 2016 Soren Mortensen. All rights reserved.
+//
+
+import Foundation
+
+public class SymondsAPITestClass {
+    
+    func printTestMessage() {
+        print("Test message!")
+    }
+    
+}
