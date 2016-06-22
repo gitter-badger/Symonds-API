71e7b27ddeacfcd07f9f53826c28b0980f445a44
Søren Mortensen <soren@sorenmortensen.com>
2016-06-22 19:01:03 +0100

    Write two basic tests

--- SymondsAPITests/SymondsAPITests.swift
+++ SymondsAPITests/SymondsAPITests.swift
@@ -9,7 +9,18 @@
 import XCTest
 @testable import SymondsAPI
 
+// MARK: Properties
+
 class SymondsAPITests: XCTestCase {
+    
+    let clientID = "29_67x28ouh3tkwco8k0ocsws0o4cc8c4s8s44gk0k444cc8ssoo8"
+    let secret = "3h33t6jpfngg8coso4w4k44k4ck40wk0kowgogggogkossws4c"
+    
+}
+
+// MARK: Life cycle
+
+extension SymondsAPITests {
     
     override func setUp() {
         super.setUp()
@@ -21,16 +32,21 @@ class SymondsAPITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+// MARK: Tests
+
+extension SymondsAPITests {
+    
+    /// Test that SymondsAPI.init(clientID:secret:redirectURL:)
+    func test_initialiser_setsClientIDCorrectly() {
+        let symondsAPI = SymondsAPI(clientID: self.clientID, secret: "", redirectURL: NSURL(string: "")!)
+        XCTAssertEqual(symondsAPI.clientID, self.clientID, "symondsAPI.clientID should have been set to self.clientID by symondsAPI.init()")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_initialiser_setsSecretCorrectly() {
+        let symondsAPI = SymondsAPI(clientID: "", secret: self.secret, redirectURL: NSURL(string: "")!)
+        XCTAssertEqual(symondsAPI.secret, self.secret, "symondsAPI.secret should have been set to self.secret by symondsAPI.init()")
     }
     
 }
