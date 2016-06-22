b9a73abb018819ea8a0b2af8d43e64349de27ebe
Søren Mortensen <soren@sorenmortensen.com>
2016-06-22 18:49:17 +0100

    Add SymondsAPI class with an initialiser and some basic properties

--- SymondsAPI/SymondsAPI.swift
+++ SymondsAPI/SymondsAPI.swift
@@ -8,4 +8,20 @@
 
 import Foundation
 
-
+public class SymondsAPI {
+    
+    public let clientID: String
+    public let secret: String
+    
+    public let redirectURL: NSURL
+    
+    public static let authURL = "https://data.psc.ac.uk/oauth/v2/auth"
+    public static let tokenUrl = "https://data.psc.ac.uk/oauth/v2/token"
+    
+    public init(clientID: String, secret: String, redirectURL: NSURL) {
+        self.clientID = clientID
+        self.secret = secret
+        self.redirectURL = redirectURL
+    }
+    
+}
