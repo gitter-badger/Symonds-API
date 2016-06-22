16627a444cc255a27ca80f18876a652815464e05
Søren Mortensen <soren@sorenmortensen.com>
2016-06-22 19:20:28 +0100

    Add documentation to SymondsAPI

--- SymondsAPI/SymondsAPI.swift
+++ SymondsAPI/SymondsAPI.swift
@@ -10,14 +10,28 @@ import Foundation
 
 public class SymondsAPI {
     
+    /// Your app's client ID
     public let clientID: String
+    /// Your app's secret
     public let secret: String
     
+    /// Your app's redirect URL
     public let redirectURL: NSURL
     
+    /// The auth URL for data.psc.ac.uk
     public static let authURL = "https://data.psc.ac.uk/oauth/v2/auth"
+    /// The token URL for data.psc.ac.uk
     public static let tokenUrl = "https://data.psc.ac.uk/oauth/v2/token"
     
+    /**
+     Initialises a SymondsAPI object with your app's keys.
+     
+     - parameter clientID: Your app's client ID (from data.psc.ac.uk).
+     - parameter secret: Your app's secret (from data.psc.ac.uk).
+     - parameter redirectURL: Your app's redirect URL (from data.psc.ac.uk).
+     
+     - returns: A SymondsAPI object initialised with a clientID, secret, and redirect URL.
+    */
     public init(clientID: String, secret: String, redirectURL: NSURL) {
         self.clientID = clientID
         self.secret = secret
