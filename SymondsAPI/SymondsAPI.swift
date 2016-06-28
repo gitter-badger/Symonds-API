baf81306836e960ae98b422056a46fcf5f0cc9c0
Søren Mortensen <soren@sorenmortensen.com>
2016-06-28 16:45:49 +0100

    Update & write a bunch of documentation

--- SymondsAPI/SymondsAPI.swift
+++ SymondsAPI/SymondsAPI.swift
@@ -8,41 +8,52 @@
 
 import Foundation
 
-/// An API for data.psc.ac.uk
-public class SymondsAPI {
+/**
+ An API that authenticates with and retrieves information from data.psc.ac.uk.
+ 
+ To begin, register your app at data.psc.ac.uk to receive your client ID and secret. Set your redirect URL to "[somescheme]://[bundleid]" where [somescheme] is an arbitrary string such as "app" or "symonds", and [bundleid] is your app's bundle ID.
+ Then initialise a SymondsAPI object with your client ID, bundle ID, and redirect URL.
+*/
+public final class SymondsAPI {
     
-    /// Your app's client ID
-    public let clientID: String
-    /// Your app's secret
-    public let secret: String
-    
-    /// Your app's redirect URL
-    public let redirectURL: String
-    
+    /// The URL used for retrieval of an access token. It is created from the base authentication URL, your app's client ID, and your app's redirect URL.
     public var authURL: URL {
         get {
+            // We're going to create a URL using a set of components
+            // Create the new URL starting with the base authentication URL
+            var components = URLComponents(string: baseAuthURL)!
+            
+            // Create the components
             let clientIDQueryItem = URLQueryItem(name: "client_id", value: clientID)
             let responseTypeQueryItem = URLQueryItem(name: "response_type", value: "code")
             let redirectURIQueryItem = URLQueryItem(name: "redirect_uri", value: redirectURL)
-            var components = URLComponents(string: baseAuthURL)!
+            
+            // Add the components to the URL and return it
             components.queryItems = [clientIDQueryItem, responseTypeQueryItem, redirectURIQueryItem]
             return components.url!
         }
     }
     
-    /// The auth URL for data.psc.ac.uk
+    /// Your app's client ID, enerated when you register an app at data.psc.ac.uk.
+    internal let clientID: String
+    /// Your app's secret, generated when you register an app at data.psc.ac.uk.
+    internal let secret: String
+    /// Your app's redirect URL; this should be set to the same value as it is on data.psc.ac.uk.
+    internal let redirectURL: String
+    
+    /// The base authentication URL for data.psc.ac.uk.
     internal let baseAuthURL = "https://data.psc.ac.uk/oauth/v2/auth"
-    /// The token URL for data.psc.ac.uk
+    /// The token URL for data.psc.ac.uk.
     internal let baseTokenUrl = "https://data.psc.ac.uk/oauth/v2/token"
     
     /**
-     Initialises a SymondsAPI object with your app's keys.
+     Creates an instance of SymondsAPI containing your app's keys.
      
-     - parameter clientID: Your app's client ID (from data.psc.ac.uk).
-     - parameter secret: Your app's secret (from data.psc.ac.uk).
-     - parameter redirectURL: Your app's redirect URL. This should also be set in your project's info.plist with the value stored on data.psc.ac.uk for your app.
+     - parameter clientID: Your app's client ID. Generated when you register an app at data.psc.ac.uk.
+     - parameter secret: Your app's secret. Generated along with your client ID.
+     - parameter redirectURL: Your app's redirect URL. You should set this when you register an app at data.psc.ac.uk. This should also be set in your project's info.plist.
      
-     - returns: A SymondsAPI object initialised with a clientID, secret, and redirect URL.
+     - returns: An instance of SymondsAPI initialised with a clientID, secret, and redirect URL.
     */
     public init(clientID: String, secret: String, redirectURL: String) {
         self.clientID = clientID
@@ -51,17 +62,22 @@ public class SymondsAPI {
     }
     
     public func getAccessToken() {
+        // Tell the application to open the authentication url in Safari, so the user can log in.
         if #available(iOS 10.0, *) {
-            UIApplication.shared().open(authURL, options: [String : AnyObject](), completionHandler: { _ in /* ... */ })
+            // We have to use a different method depending on the iOS version. This is the iOS 10 method.
+            UIApplication.shared().open(authURL)
         } else {
+            // ...and this is the pre-iOS 10 method!
             UIApplication.shared().openURL(authURL)
         }
     }
     
     public func handleCallbackURL(_ url: URL) -> Bool {
-        let query = URLComponents(url: url, resolvingAgainstBaseURL: false)?.query
-        if let codeString = query where codeString.contains("code=") {
-            return exchangeCodeForToken(codeString.components(separatedBy: "code=")[1])
+        // This method is called by AppDelegate.application(_:open:options:) in the base application when the user returns from authenticating in Safari.
+        // We will decompose the URL into its components and then its queries, to check for a query named "code" whose value is
+        let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        if let queries = components?.queryItems?.filter({ $0.name == "code" && $0.value != nil }) where queries.count == 1 {
+            return exchangeCodeForToken(queries[0].value!)
         } else { return false }
     }
     
